829988bb41841945ce68f869762421feec64c340
Gustavo Caraciolo <gustavo.ventura@jupiter.co.ao>
2022-06-09 11:00:44 +0100

    Primeiro commit - frontend

--- src/main/webapp/app/entities/reserva-quadra-tenis/update/reserva-quadra-tenis-update.component.ts
+++ src/main/webapp/app/entities/reserva-quadra-tenis/update/reserva-quadra-tenis-update.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
 import { HttpResponse } from '@angular/common/http';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
@@ -13,6 +13,8 @@ import { ReservaQuadraTenisService } from '../service/reserva-quadra-tenis.servi
 import { AlertError } from 'app/shared/alert/alert-error.model';
 import { EventManager, EventWithContent } from 'app/core/util/event-manager.service';
 import { DataUtils, FileLoadError } from 'app/core/util/data-util.service';
+import { DatePipe } from '@angular/common';
+import { NgbDate } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'jhi-reserva-quadra-tenis-update',
@@ -41,12 +43,13 @@ export class ReservaQuadraTenisUpdateComponent implements OnInit {
     protected reservaQuadraTenisService: ReservaQuadraTenisService,
     protected activatedRoute: ActivatedRoute,
     protected fb: FormBuilder
-  ) {}
+  ) {
+  }
 
   ngOnInit(): void {
-    this.activatedRoute.data.subscribe(({ reservaQuadraTenis }) => {
+    this.activatedRoute.data.subscribe(({reservaQuadraTenis}) => {
       if (reservaQuadraTenis.id === undefined) {
-        const today = dayjs().startOf('day');
+        const today = dayjs().format(DATE_TIME_FORMAT);
         reservaQuadraTenis.segundafeira = today;
         reservaQuadraTenis.tercafeira = today;
         reservaQuadraTenis.quartafeira = today;
@@ -54,10 +57,33 @@ export class ReservaQuadraTenisUpdateComponent implements OnInit {
         reservaQuadraTenis.sextafeira = today;
         reservaQuadraTenis.sabado = today;
         reservaQuadraTenis.domingo = today;
+        reservaQuadraTenis.emailDestino = 'gustavocaraciolo@gmail.com';
+        reservaQuadraTenis.templateEmail = 'Email de teste!';
       }
 
       this.updateForm(reservaQuadraTenis);
     });
+  }
+
+  onDateSelect(ngbDate: NgbDate): void {
+    const datepipe: DatePipe = new DatePipe('pt-BR');
+
+    const segundafeira = datepipe.transform(new Date(ngbDate.year, ngbDate.month - 1, ngbDate.day, 20, 0, 0), 'yyyy-MM-ddTHH:mm')
+    const tercafeira = datepipe.transform(new Date(ngbDate.year, ngbDate.month - 1, ngbDate.day + 1, 20, 0, 0), 'yyyy-MM-ddTHH:mm')
+    const quartafeira = datepipe.transform(new Date(ngbDate.year, ngbDate.month - 1, ngbDate.day + 2, 20, 0, 0), 'yyyy-MM-ddTHH:mm')
+    const quintafeira = datepipe.transform(new Date(ngbDate.year, ngbDate.month - 1, ngbDate.day + 3, 20, 0, 0), 'yyyy-MM-ddTHH:mm')
+    const sextafeira = datepipe.transform(new Date(ngbDate.year, ngbDate.month - 1, ngbDate.day + 4, 20, 0, 0), 'yyyy-MM-ddTHH:mm')
+    const sabado = datepipe.transform(new Date(ngbDate.year, ngbDate.month - 1, ngbDate.day + 5, 20, 0, 0), 'yyyy-MM-ddTHH:mm')
+    const domingo = datepipe.transform(new Date(ngbDate.year, ngbDate.month - 1, ngbDate.day + 6, 20, 0, 0), 'yyyy-MM-ddTHH:mm')
+
+    this.editForm.get(['segundafeira'])!.setValue(segundafeira);
+    this.editForm.get(['tercafeira'])!.setValue(tercafeira);
+    this.editForm.get(['quartafeira'])!.setValue(quartafeira);
+    this.editForm.get(['quintafeira'])!.setValue(quintafeira);
+    this.editForm.get(['sextafeira'])!.setValue(sextafeira);
+    this.editForm.get(['sabado'])!.setValue(sabado);
+    this.editForm.get(['domingo'])!.setValue(domingo);
+
   }
 
   byteSize(base64String: string): string {
@@ -72,7 +98,7 @@ export class ReservaQuadraTenisUpdateComponent implements OnInit {
     this.dataUtils.loadFileToForm(event, this.editForm, field, isImage).subscribe({
       error: (err: FileLoadError) =>
         this.eventManager.broadcast(
-          new EventWithContent<AlertError>('automatizacaoPessoalApp.error', { ...err, key: 'error.file.' + err.key })
+          new EventWithContent<AlertError>('automatizacaoPessoalApp.error', {...err, key: 'error.file.' + err.key})
         ),
     });
   }
@@ -116,13 +142,13 @@ export class ReservaQuadraTenisUpdateComponent implements OnInit {
       emailDestino: reservaQuadraTenis.emailDestino,
       templateEmail: reservaQuadraTenis.templateEmail,
       semana: reservaQuadraTenis.semana,
-      segundafeira: reservaQuadraTenis.segundafeira ? reservaQuadraTenis.segundafeira.format(DATE_TIME_FORMAT) : null,
-      tercafeira: reservaQuadraTenis.tercafeira ? reservaQuadraTenis.tercafeira.format(DATE_TIME_FORMAT) : null,
-      quartafeira: reservaQuadraTenis.quartafeira ? reservaQuadraTenis.quartafeira.format(DATE_TIME_FORMAT) : null,
-      quintafeira: reservaQuadraTenis.quintafeira ? reservaQuadraTenis.quintafeira.format(DATE_TIME_FORMAT) : null,
-      sextafeira: reservaQuadraTenis.sextafeira ? reservaQuadraTenis.sextafeira.format(DATE_TIME_FORMAT) : null,
-      sabado: reservaQuadraTenis.sabado ? reservaQuadraTenis.sabado.format(DATE_TIME_FORMAT) : null,
-      domingo: reservaQuadraTenis.domingo ? reservaQuadraTenis.domingo.format(DATE_TIME_FORMAT) : null,
+      segundafeira: reservaQuadraTenis.segundafeira ? reservaQuadraTenis.segundafeira : null,
+      tercafeira: reservaQuadraTenis.tercafeira ? reservaQuadraTenis.tercafeira : null,
+      quartafeira: reservaQuadraTenis.quartafeira ? reservaQuadraTenis.quartafeira : null,
+      quintafeira: reservaQuadraTenis.quintafeira ? reservaQuadraTenis.quintafeira : null,
+      sextafeira: reservaQuadraTenis.sextafeira ? reservaQuadraTenis.sextafeira : null,
+      sabado: reservaQuadraTenis.sabado ? reservaQuadraTenis.sabado : null,
+      domingo: reservaQuadraTenis.domingo ? reservaQuadraTenis.domingo : null,
     });
   }
 
